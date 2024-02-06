3175b37d9630d513de3a341daf244d583a70f600
reaulou <0juantombeng@gmail.com>
2024-02-06 14:41:23 +0700

    added product model

---
+++ src/main/java/com/reaulou/bvktechtest/model/Product.java
@@ -0,0 +1,65 @@
+package com.reaulou.bvktechtest.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Product() {
+
+    }
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+    }
+
+}
