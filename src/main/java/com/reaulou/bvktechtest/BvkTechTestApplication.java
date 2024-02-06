c090ba22272e62dc45557c5e59fd6ccfd8d102b7
reaulou <0juantombeng@gmail.com>
2024-02-06 14:04:47 +0700

    initial spring boot project

---
+++ src/main/java/com/reaulou/bvktechtest/BvkTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.reaulou.bvktechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvkTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvkTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reaulou/bvktechtest/BvkTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reaulou.bvktechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvkTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
